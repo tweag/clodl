b908703a3bcd5312032f2a2eb44a5c84d011b186
Mathieu Boespflug <m@tweag.io>
2017-02-12 22:05:19 +0100

    Reuse rawSystemExit from Distributed.Simple.Utils.
    Instead of defining our own.

--- Setup.hs
+++ Setup.hs
@@ -1,36 +1,40 @@
 import Data.Monoid
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.BuildPaths (mkSharedLibName)
+import Distribution.Simple.Setup (buildVerbosity, configVerbosity, fromFlag)
+import Distribution.Simple.Utils (rawSystemExit)
 import System.Directory (getCurrentDirectory, withCurrentDirectory)
-import System.Process (system)
 import System.Exit
 import System.FilePath ((</>))
 
 main = defaultMainWithHooks simpleUserHooks
-    { postConf = \_ _ _ _ -> configurePatchElf
-    , postBuild = \_ _ _ _ -> do
-        buildJavaSource
-        buildPatchElf
+    { postConf = \_ flags _ _ -> do
+        let verbosity = fromFlag (configVerbosity flags)
+        configurePatchElf verbosity
+    , postBuild = \_ flags _ lbi -> do
+        let verbosity = fromFlag (buildVerbosity flags)
+        buildJavaSource verbosity
+        buildPatchElf verbosity
     }
 
-configurePatchElf = do
+configurePatchElf verbosity = do
     withCurrentDirectory "vendor/patchelf" $
-      executeShellCommand "./bootstrap.sh"
+      rawSystemExit verbosity "./bootstrap.sh" []
     cwd <- getCurrentDirectory
-    executeShellCommand "mkdir -p build/src/patchelf"
+    rawSystemExit verbosity "mkdir" ["-p", "build/src/patchelf"]
     withCurrentDirectory "build/src/patchelf" $
-      executeShellCommand $
-        cwd </> "vendor/patchelf/configure --prefix=" <>
-        cwd </> "build"
+      rawSystemExit
+        verbosity
+        (cwd </> "vendor/patchelf/configure")
+        ["--prefix=" <> cwd </> "build"]
 
-buildJavaSource = do
-    executeShellCommand "gradle build"
+buildJavaSource verbosity = do
+    rawSystemExit
+      verbosity
+      "gradle"
+      ["build"]
 
-buildPatchElf = do
+buildPatchElf verbosity = do
     withCurrentDirectory "build/src/patchelf" $
-      executeShellCommand "make install"
-
-executeShellCommand cmd = system cmd >>= check
-  where
-    check ExitSuccess = return ()
-    check (ExitFailure n) =
-        error $ "Command " ++ cmd ++ " exited with failure code " ++ show n
+      rawSystemExit verbosity "make" ["install"]
