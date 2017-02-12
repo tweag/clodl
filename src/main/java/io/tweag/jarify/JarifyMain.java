10ae62fee046fd76d28d1289760ddada62bded5a
Félix Raimundo <gamaz3ps@gmail.com>
2017-02-12 13:03:30 +0100

    Introduce generic HaskellLibraryLoader (#6)
    This class should be reusable across apps. The loader should be triggered by encapsulating it in your app specific classes.

--- src/main/java/io/tweag/jarify/JarifyMain.java
+++ src/main/java/io/tweag/jarify/JarifyMain.java
@@ -1,6 +1,10 @@
 package io.tweag.jarify;
 
-public class JarifyMain extends JarifyBase {
+import io.tweag.jarify.HaskellLibraryLoader;
+
+public class JarifyMain {
+    private static HaskellLibraryLoader libraryLoader;
+
     private static native void invokeMain(String[] args);
     public static void main(String[] args) {
         invokeMain(args);
