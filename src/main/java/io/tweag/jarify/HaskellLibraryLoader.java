a70d08180e22008d94a34f2416812b6b1d801dc4
Mathieu Boespflug <m@tweag.io>
2017-02-12 21:32:22 +0100

    Indentation.

--- src/main/java/io/tweag/jarify/HaskellLibraryLoader.java
+++ src/main/java/io/tweag/jarify/HaskellLibraryLoader.java
@@ -15,56 +15,53 @@ import java.util.zip.*;
  * also loaded.
  */
 public class HaskellLibraryLoader {
-  static {
-    try {
-      InputStream in =
-        HaskellLibraryLoader.class.getResourceAsStream("/jarify-app.zip");
-      File jarifyAppZipFile =
-        File.createTempFile("jarify-app-", ".zip");
-      Files.copy(in, jarifyAppZipFile.toPath(),
-          StandardCopyOption.REPLACE_EXISTING);
-      in.close();
-      try {
-        loadApplication(jarifyAppZipFile, "hsapp");
-      } finally {
-        jarifyAppZipFile.delete();
-      }
-    } catch (Exception e) {
+    static {
+	try {
+	    InputStream in =
+		HaskellLibraryLoader.class.getResourceAsStream("/jarify-app.zip");
+	    File jarifyAppZipFile =
+		File.createTempFile("jarify-app-", ".zip");
+	    Files.copy(in, jarifyAppZipFile.toPath(),
+		       StandardCopyOption.REPLACE_EXISTING);
+	    in.close();
+	    try {
+		loadApplication(jarifyAppZipFile, "hsapp");
+	    } finally {
+		jarifyAppZipFile.delete();
+	    }
+	} catch (Exception e) {
             System.err.println(e);
-      throw new ExceptionInInitializerError(e);
+	    throw new ExceptionInInitializerError(e);
+	}
     }
-  }
 
-  private static void loadApplication(File archive, String appName)
-    throws IOException
-  {
-    // Extract all files from the .zip archive.
-    //
-    ZipFile zip = new ZipFile(archive);
-    String tmpDir = System.getProperty("java.io.tmpdir");
-    Path jarifyAppTmpDir =
-      Files.createTempDirectory(Paths.get(tmpDir), "jarify-app-");
-    ArrayList<Path> pathsList = new ArrayList();
-    try {
-      for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
-        ZipEntry entry = (ZipEntry)e.nextElement();
-        InputStream in = zip.getInputStream(entry);
-        Path path = jarifyAppTmpDir.resolve(entry.getName());
-        pathsList.add(path);
-        Files.copy(in, path);
-        in.close();
-      }
-      zip.close();
+    private static void loadApplication(File archive, String appName)
+	throws IOException
+    {
+	// Extract all files from the .zip archive.
+	ZipFile zip = new ZipFile(archive);
+	String tmpDir = System.getProperty("java.io.tmpdir");
+	Path jarifyAppTmpDir =
+	    Files.createTempDirectory(Paths.get(tmpDir), "jarify-app-");
+	ArrayList<Path> pathsList = new ArrayList();
+	try {
+	    for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
+		ZipEntry entry = (ZipEntry)e.nextElement();
+		InputStream in = zip.getInputStream(entry);
+		Path path = jarifyAppTmpDir.resolve(entry.getName());
+		pathsList.add(path);
+		Files.copy(in, path);
+		in.close();
+	    }
+	    zip.close();
 
-      // Dynamically load the app.
-      //
-      System.load(jarifyAppTmpDir.resolve(appName).toString());
-    } finally {
-      // Delete the app binary and its libraries, now that they are loaded.
-      //
-      for (Path p : pathsList)
-        p.toFile().delete();
+	    // Dynamically load the app.
+	    System.load(jarifyAppTmpDir.resolve(appName).toString());
+	} finally {
+	    // Delete the app binary and its libraries, now that they are loaded.
+	    for (Path p : pathsList)
+		p.toFile().delete();
+	}
+	jarifyAppTmpDir.toFile().delete();
     }
-    jarifyAppTmpDir.toFile().delete();
-  }
 }
