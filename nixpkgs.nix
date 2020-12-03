18cf8c45a26a047f4798ec67b18ffd3cfa16a824
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-12-03 11:14:47 +0000

    Don't use tweag fork of nixpkgs.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/tweag/nixpkgs/archive/e7ebd6be80d80000ea9efb62c589a827ba4c22dc.tar.gz")
+import (fetchTarball "https://github.com/nixos/nixpkgs/archive/e7ebd6be80d80000ea9efb62c589a827ba4c22dc.tar.gz")
