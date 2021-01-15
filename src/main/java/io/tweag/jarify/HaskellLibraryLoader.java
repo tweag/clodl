6aeb7f8515de6799a0ee6855ff30e9e622fa1b85
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-01-15 02:48:55 -0300

    Retab HaskellLibraryLoader.java

--- src/main/java/io/tweag/jarify/HaskellLibraryLoader.java
+++ src/main/java/io/tweag/jarify/HaskellLibraryLoader.java
@@ -16,68 +16,68 @@ import java.util.zip.*;
  */
 public class HaskellLibraryLoader {
 
-    public static void loadLibraries() {
-	try {
-	    InputStream in =
-		HaskellLibraryLoader.class.getResourceAsStream("/clotest.zip");
-	    File jarifyAppZipFile = File.createTempFile("jarify-app-", ".zip");
-	    Files.copy(in,
-		       jarifyAppZipFile.toPath(),
-		       StandardCopyOption.REPLACE_EXISTING);
-	    in.close();
-	    try {
-            loadApplication(jarifyAppZipFile);
-	    } finally {
-		jarifyAppZipFile.delete();
-	    }
-	} catch (Exception e) {
-            System.err.println(e);
-	    throw new ExceptionInInitializerError(e);
+	public static void loadLibraries() {
+		try {
+			InputStream in =
+			HaskellLibraryLoader.class.getResourceAsStream("/clotest.zip");
+			File jarifyAppZipFile = File.createTempFile("jarify-app-", ".zip");
+			Files.copy(in,
+				jarifyAppZipFile.toPath(),
+				StandardCopyOption.REPLACE_EXISTING);
+			in.close();
+			try {
+				loadApplication(jarifyAppZipFile);
+			} finally {
+				jarifyAppZipFile.delete();
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+			throw new ExceptionInInitializerError(e);
+		}
 	}
-    }
 
-    private static void loadApplication(File archive)
+	private static void loadApplication(File archive)
 	throws IOException
-    {
-	// Extract all files from the .zip archive.
-	ZipFile zip = new ZipFile(archive);
-	String tmpDir = System.getProperty("java.io.tmpdir");
-	Path jarifyAppTmpDir =
-	    Files.createTempDirectory(Paths.get(tmpDir), "jarify-app-");
-	ArrayList<Path> pathsList = new ArrayList();
-	try {
-	    for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
-		ZipEntry entry = (ZipEntry)e.nextElement();
-		InputStream in = zip.getInputStream(entry);
-		Path path = jarifyAppTmpDir.resolve(entry.getName());
-		pathsList.add(path);
-		Files.copy(in, path);
-		in.close();
-	    }
-	    zip.close();
+	{
+		// Extract all files from the .zip archive.
+		ZipFile zip = new ZipFile(archive);
+		String tmpDir = System.getProperty("java.io.tmpdir");
+		Path jarifyAppTmpDir =
+			Files.createTempDirectory(Paths.get(tmpDir), "jarify-app-");
+		ArrayList<Path> pathsList = new ArrayList();
+		try {
+			for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
+				ZipEntry entry = (ZipEntry)e.nextElement();
+				InputStream in = zip.getInputStream(entry);
+				Path path = jarifyAppTmpDir.resolve(entry.getName());
+				pathsList.add(path);
+				Files.copy(in, path);
+				in.close();
+			}
+			zip.close();
 
-		// Dynamically load the app.
-		String appName = "clodl-top0";
-		int i = 0;
-		do {
-			System.load(jarifyAppTmpDir.resolve(appName).toString());
-			i = i + 1;
-			appName = "clodl-top" + i;
-		} while (Files.exists(jarifyAppTmpDir.resolve(appName)));
+			// Dynamically load the app.
+			String appName = "clodl-top0";
+			int i = 0;
+			do {
+				System.load(jarifyAppTmpDir.resolve(appName).toString());
+				i = i + 1;
+				appName = "clodl-top" + i;
+			} while (Files.exists(jarifyAppTmpDir.resolve(appName)));
 
-	    // Ensure libHSjarify is loaded, since the app may or may
-	    // not depend on it, then call its init function.
-	    try (DirectoryStream<Path> stream = Files.newDirectoryStream(jarifyAppTmpDir, "libHSjarify*.so")) {
-		stream.forEach((path) -> System.load(path.toString()));
-	    }
-	    initializeHaskell("jarify-app", null);
-	} finally {
-	    // Delete the app binary and its libraries, now that they are loaded.
-	    for (Path p : pathsList)
-		p.toFile().delete();
+			// Ensure libHSjarify is loaded, since the app may or may
+			// not depend on it, then call its init function.
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(jarifyAppTmpDir, "libHSjarify*.so")) {
+				stream.forEach((path) -> System.load(path.toString()));
+			}
+			initializeHaskell("jarify-app", null);
+		} finally {
+			// Delete the app binary and its libraries, now that they are loaded.
+			for (Path p : pathsList)
+				p.toFile().delete();
+		}
+		jarifyAppTmpDir.toFile().delete();
 	}
-	jarifyAppTmpDir.toFile().delete();
-    }
 
-    private static native void initializeHaskell(String appName, String[] args);
+	private static native void initializeHaskell(String appName, String[] args);
 }
