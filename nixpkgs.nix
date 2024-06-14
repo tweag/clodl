5dbf5c3023965dc618ff9a00c8aa0546099c1c7f
Claudio Bley <claudio.bley@tweag.io>
2024-06-14 08:57:33 +0200

    Upgrade nixpkgs to nixos-24.05 and use Bazel 7

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,2 @@
-import (fetchTarball "https://github.com/nixos/nixpkgs/archive/46113713d4f25579e07b78116a61ab6f178f4153.tar.gz")
+# NixOS 24.05 (2024-06-14)
+import (fetchTarball "https://github.com/nixos/nixpkgs/archive/cc54fb41d13736e92229c21627ea4f22199fee6b.tar.gz")
